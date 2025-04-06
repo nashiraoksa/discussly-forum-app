020fe905162354a1f06366042a539e9b84baa531
nashiraoksa <nashiraaoksani@mail.ugm.ac.id>
2025-04-06 11:32:10 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-screen h-screen border border-red-500">
+      <h1 className="w-full text-center text-3xl font-bold underline border border-red-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
