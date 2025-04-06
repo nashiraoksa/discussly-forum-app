b9835ede6f7cb5263bf200a3449a9de9a31f28e7
nashiraoksa <nashiraaoksani@mail.ugm.ac.id>
2025-04-06 20:36:40 +0700

    feat: tag general component

---
+++ src/components/general/TagGeneral.jsx
@@ -0,0 +1,33 @@
+// TODO: selected & unselected tag
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function TagGeneral({ text, selected, onClick }) {
+  let styleClass = '';
+
+  switch (selected) {
+    case true:
+      styleClass += 'bg-[#373F51] text-[#D8DBE2] hover:font-medium ';
+      break;
+    case false:
+      styleClass += 'bg-[#D8DBE2] text-[#373F51] hover:font-medium ';
+      break;
+    default:
+      styleClass += 'bg-[#D8DBE2] text-[#373F51] hover:font-medium ';
+  }
+
+  return (
+    <div
+      className={`text-xs leading-1 px-4 py-2 rounded-full cursor-pointer ${styleClass}`}
+      onClick={onClick}
+    >
+      {text}
+    </div>
+  );
+}
+
+TagGeneral.propTypes = {
+  text: PropTypes.string.isRequired,
+  selected: PropTypes.bool,
+  onClick: PropTypes.func,
+};
