cd22a0cfd0fffdbb1601b48643fbba8c66b25c3d
nashiraoksa <nashiraaoksani@mail.ugm.ac.id>
2025-04-06 13:22:03 +0700

    feat: card general component

---
+++ src/components/general/CardGeneral.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function CardGeneral({ children }) {
+  return (
+    <div className='w-auto h-auto p-6 flex flex-col justify-center items-center bg-white border border-[#EAEAEA] shadow-md rounded-xl'>
+      {children}
+    </div>
+  );
+}
+
+CardGeneral.propTypes = {
+  children: PropTypes.element,
+};
