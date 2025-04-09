a0972272616da8bd3298102aaa7d5d50aa7bffaa
nashiraoksa <nashiraaoksani@mail.ugm.ac.id>
2025-04-09 08:07:38 +0700

    feat: register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import CardGeneral from '../components/general/CardGeneral';
+import RegisterInput from '../components/register/RegisterInput';
+export default function LoginPage() {
+  const onRegister = ({ name, email, password }) => {
+    console.log('register', name, email, password);
+  };
 
-export default function RegisterPage() {
-  return <div>REGISTER PAGE</div>;
+  return (
+    <div className='w-full h-full py-12 px-96 flex flex-col justify-start items-center'>
+      <CardGeneral>
+        <h2 className='text-2xl text-[#373F51] font-bold'>Register</h2>
+        <RegisterInput register={onRegister} />
+      </CardGeneral>
+    </div>
+  );
 }
