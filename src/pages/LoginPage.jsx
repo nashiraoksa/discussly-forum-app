2d687a9437016bf44edf0eef416c32c1ef2cc06a
nashiraoksa <nashiraaoksani@mail.ugm.ac.id>
2025-04-09 08:06:18 +0700

    feat: login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import CardGeneral from '../components/general/CardGeneral';
+import LoginInput from '../components/login/LoginInput';
 
 export default function LoginPage() {
-  return <div>Login Page</div>;
+  const onLogin = ({ email, password }) => {
+    console.log('login', email, password);
+  };
+
+  return (
+    <div className='w-full h-full py-12 px-96 flex flex-col justify-start items-center'>
+      <CardGeneral>
+        <h2 className='text-2xl text-[#373F51] font-bold'>Login</h2>
+        <LoginInput login={onLogin} />
+      </CardGeneral>
+    </div>
+  );
 }
