0a2c4274372a68a661da5d5c7af7e8352f48ad3b
nashiraoksa <nashiraaoksani@mail.ugm.ac.id>
2025-04-12 11:38:01 +0700

    feat: redux threads

---
+++ src/states/threads/action.js
@@ -0,0 +1,122 @@
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_THREADS: 'RECEIVE_THREADS',
+  ADD_THREAD: 'ADD_THREAD',
+  UPVOTE_THREAD: 'UPVOTE_THREAD',
+  DOWNVOTE_THREAD: 'DOWNVOTE_THREAD',
+  NEUTRALIZE_THREAD: 'NEUTRALIZE_THREAD',
+};
+
+function receiveThreadsActionCreator(threads) {
+  return {
+    type: ActionType.RECEIVE_THREADS,
+    paylaoad: {
+      threads,
+    },
+  };
+}
+
+function addThreadActionCreator(thread) {
+  return {
+    type: ActionType.ADD_THREAD,
+    payload: {
+      thread,
+    },
+  };
+}
+
+function upVoteThreadActionCreator({ threadId, userId }) {
+  return {
+    type: ActionType.UPVOTE_THREAD,
+    payload: {
+      threadId,
+      userId,
+    },
+  };
+}
+
+function downVoteThreadActionCreator({ threadId, userId }) {
+  return {
+    type: ActionType.DOWNVOTE_THREAD,
+    payload: {
+      threadId,
+      userId,
+    },
+  };
+}
+
+function neutralizeThreadActionCreator({ threadId, userId }) {
+  return {
+    type: ActionType.NEUTRALIZE_THREAD,
+    payload: {
+      threadId,
+      userId,
+    },
+  };
+}
+
+function asyncAddThread({ title, body, category }) {
+  return async (dispatch) => {
+    try {
+      const thread = await api.createThread({ title, body, category });
+      dispatch(addThreadActionCreator(thread));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+function asyncUpVoteThread(threadId) {
+  return async (dispatch, getState) => {
+    const { authUser } = getState();
+    dispatch(upVoteThreadActionCreator({ threadId, userId: authUser.id }));
+
+    try {
+      await api.upvoteThread(threadId);
+    } catch (error) {
+      alert(error.message);
+      dispatch(neutralizeThreadActionCreator({ threadId, userId: authUser.id }));
+    }
+  };
+}
+
+function asyncNeutralizeThread(threadId) {
+  return async (dispatch, getState) => {
+    const { authUser } = getState();
+    dispatch(neutralizeThreadActionCreator({ threadId, userId: authUser.id }));
+
+    try {
+      await api.neutralizeThread(threadId);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+function asyncDownVoteThread(threadId) {
+  return async (dispatch, getState) => {
+    const { authUser } = getState();
+    dispatch(downVoteThreadActionCreator({ threadId, userId: authUser.id }));
+
+    try {
+      await api.downvoteThread(threadId);
+    } catch (error) {
+      alert(error.message);
+      dispatch(neutralizeThreadActionCreator({ threadId, userId: authUser.id }));
+    }
+  };
+}
+
+export {
+  ActionType,
+  receiveThreadsActionCreator,
+  addThreadActionCreator,
+  upVoteThreadActionCreator,
+  neutralizeThreadActionCreator,
+  downVoteThreadActionCreator,
+  asyncAddThread,
+  asyncUpVoteThread,
+  asyncNeutralizeThread,
+  asyncDownVoteThread,
+};
