51a94c7cd07f8a607123256ea0357aa9b0bd4292
nashiraoksa <nashiraaoksani@mail.ugm.ac.id>
2025-04-13 17:07:02 +0700

    feat: handle vote detail thread

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,63 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CardGeneral from '../components/general/CardGeneral';
 import ThreadDetail from '../components/thread/ThreadDetail';
 import CommentInput from '../components/comment/CommentInput';
 import CommentList from '../components/comment/CommentList';
+import { useParams } from 'react-router-dom';
+import {
+  asyncReceiveThreadDetail,
+  asyncUpVoteThreadDetail,
+  asyncDownVoteThreadDetail,
+  asyncNeutralizeThreadDetail,
+} from '../states/threadDetail/action';
+import { asyncAddComment } from '../states/comments/action';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function DetailPage() {
-  const detailThread = {
-    id: 'thread-1',
-    title: 'Thread Pertama',
-    body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum cupiditate sunt commodi quis eius animi dolorem consequuntur earum asperiores doloremque, hic quas odit. At ipsam magnam officiis doloremque molestiae eveniet! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum cupiditate sunt commodi quis eius animi dolorem consequuntur earum asperiores doloremque, hic quas odit. At ipsam magnam officiis doloremque molestiae eveniet! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum cupiditate sunt commodi quis eius animi dolorem consequuntur earum asperiores doloremque, hic quas odit. At ipsam magnam officiis doloremque molestiae eveniet! ',
-    category: 'General',
-    createdAt: '2021-06-21T07:00:00.000Z',
-    owner: {
-      id: 'users-1',
-      name: 'John Doe',
-      avatar: 'https://generated-image-url.jpg',
-    },
-    upVotesBy: [],
-    downVotesBy: [],
-    comments: [
-      {
-        id: 'comment-1',
-        content: 'Ini adalah komentar pertama',
-        createdAt: '2021-06-21T07:00:00.000Z',
-        owner: {
-          id: 'users-1',
-          name: 'John Doe',
-          avatar: 'https://generated-image-url.jpg',
-        },
-        upVotesBy: [],
-        downVotesBy: [],
-      },
-    ],
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const { threadDetail = null, authUser } = useSelector((states) => states);
+
+  const onUpVoteThread = () => {
+    if (threadDetail.upVotesBy.includes(authUser?.id)) {
+      dispatch(asyncNeutralizeThreadDetail());
+    } else {
+      dispatch(asyncUpVoteThreadDetail());
+    }
   };
 
+  const onDownVoteThread = () => {
+    if (threadDetail.downVotesBy.includes(authUser?.id)) {
+      dispatch(asyncNeutralizeThreadDetail());
+    } else {
+      dispatch(asyncDownVoteThreadDetail());
+    }
+  };
+
+  const onCommentThread = () => {};
+
+  const onVoteComment = () => {};
+
+  useEffect(() => {
+    dispatch(asyncReceiveThreadDetail(id));
+  }, [id, dispatch]);
+
+  if (!threadDetail) {
+    return null;
+  }
+
   return (
-    // <div>
     <div className='w-full h-full py-12 px-96 flex flex-col justify-start items-center'>
-      {/* TODO: props detail object spread params */}
       <CardGeneral>
         <div className='w-full flex flex-col gap-6'>
-          {/* talk detail */}
           <ThreadDetail
-            id={detailThread.id}
-            title={detailThread.title}
-            body={detailThread.body}
-            category={detailThread.category}
-            createdAt={detailThread.createdAt}
-            username={detailThread.owner.name}
-            avatar={detailThread.owner.avatar}
-            upVotesBy={detailThread.upVotesBy}
-            downVotesBy={detailThread.downVotesBy}
+            {...threadDetail}
+            authUser={authUser}
+            onUpVoteThread={onUpVoteThread}
+            onDownVoteThread={onDownVoteThread}
           />
           {/* TODO: render reply comment input only if authUser exist */}
-          {/* <div> */}
-          {/* comment section */}
-          <CommentInput />
-          <CommentList />
-          {/* </div> */}
+          <CommentInput onCommentThread={onCommentThread} />
+          <CommentList onVoteComment={onVoteComment} />
         </div>
       </CardGeneral>
     </div>
