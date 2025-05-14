f09ab5954d2136cbb734491bb6d06926f0f099f8
nashiraoksa <nashiraaoksani@mail.ugm.ac.id>
2025-05-14 09:58:14 +0700

    feat: e2e login flow

--- src/components/main/Navigation.jsx
+++ src/components/main/Navigation.jsx
@@ -12,7 +12,7 @@ export default function Navigation({ authUser, signOut }) {
   const location = useLocation();
 
   return (
-    <div className='w-full h-[64px] flex justify-between bg-[#373F51] border-b border-b-[#A9BCD0] border-opacity-50 text-white fixed top-0'>
+    <div className='w-full h-[64px] flex justify-between bg-[#373F51] border-b border-b-[#A9BCD0] border-opacity-50 text-white fixed top-0 z-20'>
       <Link to='/' className='w-1/3 pl-16 flex gap-2 items-center'>
         <img src={DiscusslyLogo} alt='DiscussLy_Logo' className='w-[35px]' />
         <h1 className='text-xl font-semibold'>DiscussLy</h1>
